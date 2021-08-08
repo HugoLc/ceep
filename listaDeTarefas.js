88d832332f3bdfe03a9cff1af1b830f3c4c8d637
Hugo LC <hugolindoso.c@gmail.com>
2021-08-08 12:19:22 -0300

    Adicionada uma função para marcar uma tarefa como concluida

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,5 +1,6 @@
 const novaTarefa = document.querySelector('[data-form-bt]');
 const tbTarefa = document.querySelector('[data-form-tb]');
+var cont = 0;
 
  // metodo listener para o evento click chamado uma função anonima que executa um console.log
 // novaTarefa.addEventListener('click', ()=> {console.log('fui clicado')})
@@ -8,17 +9,18 @@ novaTarefa.addEventListener('click', ()=>{criarTarefa(tbTarefa)});
 function criarTarefa(textBox) {
   var texto = getTextoTarefa(textBox);
   console.log(texto);
-  var conteudo = `<li class="task">
-                    <p class="content">
+  var conteudo = `<li id="li-${cont}" class="task">
+                    <p id="p-${cont}" class="content">
                       ${texto}
                     </p>
                     <div class="acoes-tarefa">
-                      <i class="fas fa-check"></i>
-                      <i class="fas fa-trash"></i>
+                      <i onclick="concluirTarefa('p-${cont}')" class="fas fa-check"></i>
+                      <i onclick="excluirTarefa('li-${cont}')" class="fas fa-trash"></i>
                     </div>
                   </li>` // estudar template string
   var lista = document.querySelector('[data-lista]');
   lista.innerHTML += conteudo;
+  cont++;
 }
 
 function getTextoTarefa(textBox){
@@ -26,4 +28,15 @@ function getTextoTarefa(textBox){
   console.log(texto);
   textBox.value = ''; // limpar campo
   return texto;
+}
+
+function concluirTarefa(id){
+  console.log(id);
+  var elemento = document.getElementById(id);
+  var textoP = elemento.innerHTML;
+  elemento.innerHTML = textoP.strike();
+}
+
+function excluirTarefa(id){
+  console.log(id);
 }
