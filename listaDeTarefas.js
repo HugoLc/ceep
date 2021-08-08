0e954fc8044a6979934be928834c472e858097df
Hugo LC <hugolindoso.c@gmail.com>
2021-08-08 10:27:35 -0300

    Primeiro js!!! Selecionado o botão de novo item que adicionada uma ação para quando for clicado

---
+++ listaDeTarefas.js
@@ -0,0 +1,3 @@
+const novaTarefa = document.querySelector('[data-form-bt]');
+ // metodo listener para o evento click chamado uma função anonima que executa um console.log
+novaTarefa.addEventListener('click', ()=> {console.log('fui clicado')})
