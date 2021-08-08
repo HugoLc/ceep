e4bc03941d0ea551a6fe7b5beef208f53932cdb5
Hugo LC <hugolindoso.c@gmail.com>
2021-08-08 11:07:38 -0300

    criada uma função para pegar o texto de dentro do input

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,3 +1,13 @@
 const novaTarefa = document.querySelector('[data-form-bt]');
+const tbTarefa = document.querySelector('[data-form-tb]');
+
  // metodo listener para o evento click chamado uma função anonima que executa um console.log
-novaTarefa.addEventListener('click', ()=> {console.log('fui clicado')})
+// novaTarefa.addEventListener('click', ()=> {console.log('fui clicado')})
+novaTarefa.addEventListener('click', ()=>{getTextoTarefa(tbTarefa)});
+
+
+function getTextoTarefa(textBox){
+  var texto = textBox.value;
+  console.log(texto);
+  textBox.value = '';
+}
