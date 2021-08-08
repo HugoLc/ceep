32bb013df90c726c9c1705b6a3e8beeb75940ab1
Hugo LC <hugolindoso.c@gmail.com>
2021-08-08 11:52:21 -0300

    criada a função para criar nova tarefa adicionando uma nova li ao conteudo

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -3,11 +3,27 @@ const tbTarefa = document.querySelector('[data-form-tb]');
 
  // metodo listener para o evento click chamado uma função anonima que executa um console.log
 // novaTarefa.addEventListener('click', ()=> {console.log('fui clicado')})
-novaTarefa.addEventListener('click', ()=>{getTextoTarefa(tbTarefa)});
+novaTarefa.addEventListener('click', ()=>{criarTarefa(tbTarefa)});
 
+function criarTarefa(textBox) {
+  var texto = getTextoTarefa(textBox);
+  console.log(texto);
+  var conteudo = `<li class="task">
+                    <p class="content">
+                      ${texto}
+                    </p>
+                    <div class="acoes-tarefa">
+                      <i class="fas fa-check"></i>
+                      <i class="fas fa-trash"></i>
+                    </div>
+                  </li>` // estudar template string
+  var lista = document.querySelector('[data-lista]');
+  lista.innerHTML += conteudo;
+}
 
 function getTextoTarefa(textBox){
   var texto = textBox.value;
   console.log(texto);
-  textBox.value = '';
+  textBox.value = ''; // limpar campo
+  return texto;
 }
